73127bb3e9ef846ea87bf0d837e247517f90928c
kanhrithfz <kanhrith.non@gmail.com>
2019-03-07 11:35:40 -0800

    test home activity with using google maps

--- app/src/main/java/com/kanhrithnon/khmerdeliverapp/HomeActivity.java
+++ app/src/main/java/com/kanhrithnon/khmerdeliverapp/HomeActivity.java
@@ -3,11 +3,38 @@ package com.kanhrithnon.khmerdeliverapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng phnompenh = new LatLng(11.531442, 104.929640);
+        mMap.addMarker(new MarkerOptions().position(phnompenh).title("Tonle Basak Restaurant"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(phnompenh,15f));
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
